ef2ff1f87bd53710512c361a0b60b377d4d907db
ksh277 <ksh277@example.com>
2025-09-07 14:48:51 +0900

    Apply recent changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   images: {
     domains: ['via.placeholder.com', 'placehold.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https', 
+        hostname: 'via.placeholder.com',
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
